7426b4428d01681684c09205977f9806cee99f85
kylepg <kylepgeorge@gmail.com>
2018-01-03 14:18:14 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,142 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    /*=============================
+    =            WATCH            =
+    =============================*/
+
+    watch: {
+      html: {
+        files: ['src/standings.html',
+                'src/html/*.html'],
+        tasks: ['htmlmin', 'import','notify:done']
+      },
+      js: {
+        files: ['src/js/*.js'],
+        tasks: ['browserify', 'import', 'notify:done', 'uglify']
+      },
+      css: {
+        files: ['src/scss/*.scss',
+                'src/scss/mixins/*.scss'],
+        tasks: ['sass', 'import','notify:done']
+      }
+    },
+
+    /*===================================
+    =            MINIFY HTML            =
+    ===================================*/
+
+    htmlmin: {
+       dist: {
+         options: {
+           gruntLogHeader: false,
+           removeComments: true,
+           collapseWhitespace: true
+         },
+         files: {
+           'src/html/min/template.min.html': 'src/html/template.html' // CHANGE TEMPLATE NAME
+         }
+       }
+     },
+
+     /*====================================
+     =            COMPILE SASS            =
+     ====================================*/
+
+    sass: {
+      dist: {
+        options: {
+          gruntLogHeader: false,
+          sourcemap: 'none',
+        },
+        files: {
+          'dist/css/standings.css': 'src/scss/standings.scss'
+        }
+      },
+      min: {
+        options: {
+          gruntLogHeader: false,
+          sourcemap: 'none',
+          style: 'compressed'
+        },
+        files: {
+          'dist/css/standings.min.css': 'src/scss/standings.scss'
+        }
+      }
+    },
+
+    /*=========================================
+    =            UGLIFY JAVASCRIPT            =
+    =========================================*/
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/js/standings.min.js': 'dist/js/standings.js'
+        }
+      }
+    },
+
+    /*==============================
+    =            IMPORT            =
+    ==============================*/
+
+    import: {
+      options: {
+        gruntLogHeader: false
+      },
+      dist: {
+        files: {
+          'dist/js/standings.js' : 'src/js/standings.js',
+          'dist/standings.ready.html' : 'src/standings.html'
+        }
+      }
+    },
+
+    /*==================================
+    =            browserify            =
+    ==================================*/
+
+    browserify: {
+        dev: {
+            src: [
+                "src/js/standings.js"
+            ],
+            dest: 'dist/js/standings.js',
+            options: {
+                browserifyOptions: { debug: true },
+                transform: [["babelify", { "presets": ["env"] }]],
+            }
+        }
+    },
+
+    /*==============================
+    =            NOTIFY            =
+    ==============================*/
+
+    notify: {
+      done: {
+        options: {
+          gruntLogHeader: false,
+          title: 'Grunt - standings',
+          message: 'DONE!',
+        }
+      }
+    }
+  });
+
+  /*==================================
+  =            LOAD TASKS            =
+  ==================================*/
+
+  require('grunt-log-headers')(grunt);
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-import');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.registerTask('default',['watch']);
+};
